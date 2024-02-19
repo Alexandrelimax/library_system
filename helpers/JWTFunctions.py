936aaf651d70c173abc34386bb9733f6fe9f4302
Alexandre <alexandrelimaxxxx@gmail.com>
2024-02-19 16:47:53 -0300

    SQL code in helpers

--- helpers/JWTFunctions.py
+++ helpers/JWTFunctions.py
@@ -2,6 +2,7 @@ import jwt
 from datetime import datetime, timedelta, timezone
 from app import app
 
+
 def generate_token(id, user):
     expiration_token = datetime.now(timezone.utc) + timedelta(hours=5)
     return jwt.encode({'id': id, 'username': user, 'exp': expiration_token}, app.config['secret'], algorithm='HS256')
