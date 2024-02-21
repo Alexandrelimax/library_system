08f59061057dde3c607f19fe1753713bed7da133
Alexandre <alexandrelimaxxxx@gmail.com>
2024-02-21 16:25:22 -0300

    update 2 endpoints to acept csv file

--- app/controllers/books_controller.py
+++ app/controllers/books_controller.py
@@ -1,4 +1,6 @@
 import json
+import os.path
+
 import jwt
 from app import app
 from flask import Blueprint, jsonify, request, Response
@@ -6,7 +8,17 @@ from app.repositories import BooksRepository
 from app.services import BooksServices
 from app.repositories import CategoryRepository
 from database import Database
+
 books_controller = Blueprint('books', __name__)
+from werkzeug.utils import secure_filename
+import os
+from app import app
+UPLOAD_FOLDER = 'C:/Users/alexandre.lima_ipnet/PycharmProjects/library1/upload/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+import csv
+from flask import send_file, send_from_directory
+# UPLOAD_FOLDER = os.path.join(os.getcwd(), 'upload')
+
 
 
 @books_controller.route('/', methods=['GET'])
@@ -14,8 +26,10 @@ def get_all_books():
     conn = Database.init_connection()
     books_service = BooksServices(conn)
     books = books_service.get_all_books()
+    print(books)
     # return jsonify(books)
     return Response(json.dumps(books), content_type='application/json charset=utf-8')
+
 
 @books_controller.route('/add_book', methods=['POST'])
 def insert_new_book():
@@ -69,7 +83,6 @@ def get_book(id_book):
     return Response(json.dumps(book), content_type='application/json; charset=utf-8')
 
 
-
 @books_controller.route('/rent/<int:id_book>', methods=['POST'])
 def rent_a_book(id_book):
     connection = Database.init_connection()
@@ -110,17 +123,53 @@ def return_book(id_book):
         return jsonify({'Acesso negado': 'Token inválido'})
 
 
+@books_controller.route('/csv/', methods=['POST'])
+def upload_file_by_csv():
+    connection = Database.init_connection()
+    book_repo = BooksRepository(connection)
+    category_repo = CategoryRepository(connection)
+
+    file = request.files['files']
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
+    save_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
+    file.save(save_path)
+
+    with open(save_path, 'r') as new_file:
+        data = new_file.read()
+        lines = data.split('\n')
+        content_list = [row.split(',') for row in lines]
+
+        for index, row in enumerate(content_list):
+            if index != 0:
+                print(row)
+                name, description, url_img, author, category = row
+                print(name, description, url_img, author, category)
+
+                id_book = book_repo.insert_book(name, description, url_img, author)
+
+                id_category = category_repo.get_category(category)
+                book_repo.associate_book_by_category(id_book, id_category)
 
 
+    return jsonify({'message': 'recebido'})
 
 
+@books_controller.route('/download/', methods=['GET'])
+def send_file_by_csv():
+    conn = Database.init_connection()
+    books_service = BooksServices(conn)
+    books = books_service.get_all_books()
 
+    with open(f'{UPLOAD_FOLDER}/downloadfile.csv', 'w') as file:
+        writer = csv.writer(file)
+        header = books[0].keys()
+        writer.writerow(tuple(header))
+        for book in books:
+            writer.writerow((book['id'], book['name'], book['description'], book['url_img'], book['status'], book['author'], book['created_at']))
 
-
-
-
-
-
+    return send_file(f'{app.config['UPLOAD_FOLDER']}/downloadfile.csv', as_attachment=True)
 
 
 
